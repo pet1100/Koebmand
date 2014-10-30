3240ef55cd56d68d2c03cba0118350354af1f202
pet1100 <peterbrj@gmail.com>
2014-10-30 12:05:34 +0100

    Update and rename Vare.java to Goods.java

---
+++ Goods.java
@@ -0,0 +1,15 @@
+Package Koebmand;
+
+public Class Goods
+{
+  private int ID;
+  private String name;
+  private int price;
+  
+  public Goods(int ID, String name, int price)
+  {
+    this.ID = ID;
+    this.name = name;
+    this.price = price;
+  }
+}
