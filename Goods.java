d66ffa961a90a32d3108b9ba84ed888830666f41
navoi <lucky.musen1@gmail.com>
2014-10-30 12:28:32 +0100

    Update Goods.java

--- Goods.java
+++ Goods.java
@@ -7,11 +7,12 @@ public Class Goods implements Serializable
   private int ID;
   private String name;
   private int price;
-  
+ 
   public Goods(int ID, String name, int price)
   {
     this.ID = ID;
     this.name = name;
     this.price = price;
+   
   }
 }
