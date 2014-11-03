3849ef5396e8ed2b0bb97c50a49f6111bb03a25a
pet1100 <peterbrj@gmail.com>
2014-11-03 09:55:12 +0100

    Update GUIStorage.java

--- GUIStorage.java
+++ GUIStorage.java
@@ -5,11 +5,18 @@ public class GUIStorage
   
   private JFrame jf = new JFrame();
   private Storage stored = new Storage();
+  private ArrayList<Storage> storList = new Arraylist<>(); 
   
-  GUIStorage()
+  public GUIStorage
   {
+  	
+  }
+  
+  public void makeGUI()
+  {		
+  		storList.addAll(stored.getList());
 		jf.setVisible(true);
 		jf.setSize(500, 500);
-    jf.setResizable(false);    
+		jf.setResizable(false);    
   }
 }
