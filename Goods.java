b90b55cf1ff1c29e780725f15dd8692f3ec4518a
pet1100 <peterbrj@gmail.com>
2014-11-03 10:04:10 +0100

    Update Goods.java

--- Goods.java
+++ Goods.java
@@ -7,13 +7,28 @@ public Class Goods implements Serializable
   private int ID;
   private String name;
   private int price;
+  private int quantity;
  
   public Goods(int ID, String name, int price)
   {
     this.ID = ID;
     this.name = name;
     this.price = price;
-   
+  }
+  
+  public int getQuantity()
+  {
+    return quantity;
+  }
+  
+  public void addQuantity(int addQuantity)
+  {
+    quantity += addQuantity;
+  }
+  
+  public void removeQuantity()
+  {
+    quantity--;
   }
   
   public int getID()
