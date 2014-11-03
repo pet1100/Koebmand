e1dacf4b2cd39953b4e2a08e81565af385efd8aa
pet1100 <peterbrj@gmail.com>
2014-11-03 09:24:25 +0100

    Update GUIStorage.java

--- GUIStorage.java
+++ GUIStorage.java
@@ -2,7 +2,14 @@
 
 public class GUIStorage
 {
+  
+  private JFrame jf = new JFrame();
+  private Storage stored = new Storage();
+  
   GUIStorage()
   {
+		jf.setVisible(true);
+		jf.setSize(500, 500);
+    jf.setResizable(false);    
   }
 }
