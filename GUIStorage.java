c0dffd81b1faa122f4673f6a95f63c900590d650
pet1100 <peterbrj@gmail.com>
2014-11-05 13:31:39 +0100

    Update GUIStorage.java

--- GUIStorage.java
+++ GUIStorage.java
@@ -1,19 +1,9 @@
 package Koebmand;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 
 public class GUIStorage
